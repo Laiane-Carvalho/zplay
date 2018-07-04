51d19b4180cfff3dfc2e25e12687925337580bb0
Wellton S. Barros <cafecanudo@gmail.com>
2018-07-04 16:45:50 -0300

    Comit initial

---
+++ app/src/androidTest/java/zup/com/br/zplay/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package zup.com.br.zplay;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("zup.com.br.zplay", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/zup/com/br/zplay/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package zup.com.br.zplay.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zup.com.br.zplay.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
