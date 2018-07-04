2c3fd6ff9fd4e1101d22f2f36b2e835d841e2fe5
Wellton S. Barros <cafecanudo@gmail.com>
2018-07-04 16:58:06 -0300

    Comit initial

--- app/src/main/java/zup/com/br/zplay/activities/SplashActivity.java
+++ app/src/main/java/zup/com/br/zplay/activities/SplashActivity.java
@@ -1,15 +1,33 @@
 package zup.com.br.zplay.activities;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import zup.com.br.zplay.MainActivity;
 import zup.com.br.zplay.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int TEMPO_SPLASH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        this.ativarSplash();
+    }
+
+    private void ativarSplash() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TEMPO_SPLASH);
     }
 }
