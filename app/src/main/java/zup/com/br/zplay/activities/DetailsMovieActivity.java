56f3fcef870beaae17e107529fb4b5feac75c613
Wellton S. Barros <cafecanudo@gmail.com>
2018-07-10 10:50:43 -0300

    Add autor

--- app/src/main/java/zup/com/br/zplay/activities/DetailsMovieActivity.java
+++ app/src/main/java/zup/com/br/zplay/activities/DetailsMovieActivity.java
@@ -9,6 +9,9 @@ import butterknife.BindView;
 import butterknife.OnClick;
 import zup.com.br.zplay.R;
 
+/**
+ * Autor Wellton
+ */
 public class DetailsMovieActivity extends SupportActivity {
 
     @BindView(R.id.imagem)
